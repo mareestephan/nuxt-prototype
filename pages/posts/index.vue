23dbc0f8df5138f127bd130dad8cf5ddf5f94dc3
mareestephan <stephanmaree@gmail.com>
2020-07-15 10:53:29 +0200

    first commit

---
+++ pages/posts/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Posts page INDEX</h1>
+
+        <li v-for="post in posts" :key="post.id" > 
+<nuxt-link :to="`/posts/${post.slug}`"> {{post.meta_title}} </nuxt-link>
+        </li>
+
+    </div>
+</template>
+
+<script>
+export default {
+  asyncData({ $axios }) {
+    return (
+      $axios
+        // .get("http://35.190.35.153/system-api/pages-to-create/all-sale")
+        .get("http://35.190.35.153/system-api/pages-to-create/all-sale")
+        // .get(
+        //   "https://my-json-server.typicode.com/mareestephan/jsonProducts/pages_to_create"
+        // )
+        .then(response => {
+          // console.log(response.data);
+          return { posts: response.data.pages_to_create };
+        })
+    );
+  },
+  data() {}
+};
+</script>
+
+<style>
+</style>
---
+++ pages/posts/ssr/_slug.vue
@@ -0,0 +1,38 @@
+<template>
+    
+<div>
+
+  
+    <h1>this is blog number {{$route.params.slug}}</h1>
+    <!-- <h1>{{post[0].meta_title}}</h1>
+    <a :href="`${post[0].link}`" :key='post[0].meta_title'>{{post[0].link}}</a>
+    <pre>{{post}}</pre> -->
+
+ <li v-for="post in posts" :key="post.product_id" > 
+<a :href="`${post.product_link}`"> {{post.product_link}} </a>
+        </li>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  asyncData({ $axios, route }) {
+    // console.log(route.params.category_id);
+    return $axios
+      .get(
+        `http://35.190.35.153/system-api/pages-to-create/all-sale?slug=${
+          route.params.slug
+        }`
+      )
+      .then(response => {
+        // console.log(response.data.pages_to_create[0].matched_products);
+        return { posts: response.data.pages_to_create[0].matched_products };
+      });
+  }
+};
+</script>
+
+<style>
+</style>
